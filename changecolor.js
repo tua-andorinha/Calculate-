84421231c5f7d89a22cc79509c289b765821b201
tua-andorinha <155818724+tua-andorinha@users.noreply.github.com>
2024-02-23 05:07:20 -0800

    Add files via upload
    simple javascript code to change the background

---
+++ changecolor.js
@@ -0,0 +1,6 @@
+var blueButton = document.querySelector("button");
+var body = document.querySelector("body");
+
+addEventListener("click", function () {
+  body.style.backgroundColor = "blue";
+});
